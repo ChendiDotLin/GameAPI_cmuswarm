23e55401f81b5232d18ed59676077972d440e94c
Turtle Lin <chendilin92@gmail.com>
2019-02-05 12:59:55 -0500

    circle

--- circle.py
+++ circle.py
@@ -1,10 +1,10 @@
 import requests
 import time
 import numpy as np
-import json
+
 
 ##### parameters
-connectivity_radius = 20.0
+radius = 20.0
 max_linear_velocity = 2.0
 
 ##### get position of all robots
@@ -13,42 +13,49 @@ get_req = requests.get(url=get_URL)
 
 
 p_data = get_req.json()
-print (p_data)
+print p_data
 num_robots = len(p_data)
-print (num_robots)
+print num_robots
 
 positions = []
 for i in range(num_robots):
 	positions.append([p_data[i][u'x'],p_data[i][u'y'],p_data[i][u'z']])
 centroid = sum(np.array(positions))/num_robots
 
-
+pie = 2*np.pi/num_robots
 
 
 
 ##### send target position of all robots
-set_URL = "http://localhost:1234/Vehicle/Position/XYZ/Multi"
-# set_URL = "http://localhost:1234/Vehicle/Position/LLA/Multi"
+set_URL = "http://localhost:1234/Vehicle/Position/XYZ/Single"
 goal = []
 for i in range(num_robots):
-	goal.append({"id":i,"x":22,"y":33,"z":44})
-	# goal.append({"id":i,"data":{"lat":37,"lon":-84,"alt":270}})
-	# goal.append({"id":i,"MovArgs":{"speed":10,"distThresh":1.0},"x":centroid[0],"y":centroid[1],"z":centroid[2]})
+	# goal.append({"id":i,"MovArgs":{"speed":10,"distThresh":1.0},"X":centroid[0],"Y":centroid[1],"Z":centroid[2]})
 
 # xyz_data = {"id":0,"x":45.500, "y":72.3000,"z":10.000}
-	# data = {"id":i,"x":centroid[0], "y":centroid[1],"z":centroid[2]}
-	# print i
+	# data = {"id":i,"x":centroid[0], "y":centroid[1],"z":centroid[2],"MovArgs":{"speed":10, "distThresh":0.0}}
+	data = {"id":i,"x":30+radius*np.cos(pie*i), "y":30+radius*np.sin(pie*i),"z":40,"MovArgs":{"speed":10, "distThresh":0.0}}
+
+	print i
 #lla_data = {"id":1,"data":{"lat":37.46832, "lon":-84.2317047,"alt":311.3}}
-margs = {"speed":1, "distThresh":1}
-data_paras = {"positions":goal,"MovArgs":margs}
-print(data_paras["positions"][0])
-print(data_paras["MovArgs"])
-print (data_paras)
-# data_paras = json.dumps(data_paras)
+# data_paras = {"positions":goal}
 # data_paras = {"positions":[{"id":0, "MovArgs":{"speed":10, "distThresh":1.0}, "X":309.29, "Y":354, "Z":-107},{"id":1, "MovArgs":{"speed":5, "distThresh":1.0},"X":406,  "Y":354, "Z":-230}, {"id":2, "MovArgs":{"speed":20, "distThresh":1.0},  "X":358,  "Y":374,  "Z":-351}]}
-	# print(data)
-set_req = requests.post(url = set_URL, data = data_paras)
-print (set_req.status_code,'\n',set_req.reason)
+	print(data)
+	set_req = requests.post(url = set_URL, data = data)
+	print set_req.status_code,'\n',set_req.reason
 	# set_req = requests.post(url = set_URL, data = data)
 	# print set_req.status_code,'\n',set_req.reason
 
+time.sleep(10)
+
+get_req = requests.get(url=get_URL)
+
+data = get_req.json()
+print data
+
+velocities = [[0,0,0]]*num_robots
+print velocities
+# while True:
+# 	for i in range(num_robots):
+		# i is the id of each robot
+		
--- web_req_test.py
+++ web_req_test.py
@@ -1,22 +1,25 @@
 import requests
 import time
+import numpy as np
 
 ##### get position of a single robot
-get_URL = "http://localhost:1234/Vehicle/Position/XYZ/index=0"
+get_URL = "http://localhost:1234/Vehicle/Position/XYZ/ALL"
 get_req = requests.get(url=get_URL)
 
 
 data = get_req.json()
 print data
 
+num_robot = len(data)
+print(num_robot)
 
 
 
 ##### send target position of a single robot
 set_URL = "http://localhost:1234/Vehicle/Position/XYZ/Single"
 
-for i in range(3):
-    xyz_data = {"id":i,"x":15*i, "y":17*i,"z":13*i,"MovArgs":{"speed":10, "distThresh":0.0}}
+for i in range(num_robot):
+    xyz_data = {"id":i,"x":20*np.sin(i+1), "y":20*np.cos(i),"z":20*np.sin(i),"MovArgs":{"speed":10, "distThresh":1.0}}
 #lla_data = {"id":1,"data":{"lat":37.46832, "lon":-84.2317047,"alt":311.3}}
 
     set_req = requests.post(url = set_URL, data = xyz_data)
